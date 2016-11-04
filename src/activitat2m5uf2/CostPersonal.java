232c88e7d7fcc7dded61710cd2edac907f3e70a2
ericdote <pokemon13>
2016-11-04 20:04:02 +0100

    Activitat 2 Part 2 Feta.

--- src/activitat2m5uf2/CostPersonal.java
+++ src/activitat2m5uf2/CostPersonal.java
@@ -10,11 +10,18 @@ package activitat2m5uf2;
  * @author ALUMNEDAM
  */
 public class CostPersonal {
-
+    //Variable que initzialitzem en global per poder usarla als altres metodes
     private static float costFinal = 0;
-    private static int horasExtras = 20;
+    //Variable final global per les hores extres
+    private static final int horasExtras = 20;
     private static Treballador treballador = null;
-
+    /**
+     * Li entra un array de treballadors amb els diferents treballadors amb els seus atributs.
+     * Un cop li entra realitza un array per anar comprovant cada treballador i sumar el costFinal
+     * I retorna el costFinal
+     * @param treballadors
+     * @return 
+     */
     public static float CostDelPersonal(Treballador treballadors[]) {
         for (int i = 0; i < treballadors.length; i++) {
             treballador = treballadors[i];
@@ -23,7 +30,11 @@ public class CostPersonal {
         }
         return costFinal;
     }
-
+    /***
+     * Li arriba el treballador que envia el metode CostDelPersonal per veure quin tipus de treballador es i retorna el tipus.
+     * @param treballador
+     * @return 
+     */
     public static int tipusTreballador(Treballador treballador) {
         int tipus;
         if (treballador.getTipusTreballador() == Treballador.DIRECTOR) {
@@ -35,7 +46,13 @@ public class CostPersonal {
         }
         return tipus;
     }
-
+    /**
+     * Li entra el treballador que envia el metode CostDelPersonal i el tipus de treballador per calcular el costFinal
+     * I retorna el costFinal.
+     * @param treballador
+     * @param tipus
+     * @return 
+     */
     public static float costFinal(Treballador treballador, int tipus) {
         if (tipus == Treballador.DIRECTOR || tipus == Treballador.SUBDIRECTOR) {
             costFinal = costFinal + treballador.getNomina();
